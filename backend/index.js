6c5774b660e8391885d3e09edb3518ea74c92fb2
Himanshu <hv65616@gmail.com>
2022-01-23 00:03:45 +0530

    creared router for smooth and ease working of api

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,10 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/auth", require("./routes/auth"));
+
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
-
