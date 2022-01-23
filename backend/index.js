a168008d926ae61abf986260f98d81c198724072
Himanshu <hv65616@gmail.com>
2022-01-23 21:00:35 +0530

    sending a post request and adding the data in database

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 connectToMongo();
 const app = express();
 const port = 3000;
-
+app.use(express.json())
 app.use("/api/auth", require("./routes/auth"));
 
 app.use("/api/notes", require("./routes/notes"));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User")
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
+//Create A User Using POST Request
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body)
+  user.save()
+  res.send(req.body);
 });
 module.exports = router;
