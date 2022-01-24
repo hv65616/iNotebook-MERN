134349683863adff4ad739ccbac79fd8c7d8ef3b
Himanshu <hv65616@gmail.com>
2022-01-24 21:51:07 +0530

    adding data in inotebook database

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+  "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("Connected To Mongoose");
   });
 };
-module.exports = connectToMongo
+module.exports = connectToMongo;
