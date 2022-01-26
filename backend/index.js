ddbb4c8fc3171db48c5eea63e74c4f117d735482
Himanshu <hv65616@gmail.com>
2022-01-26 23:22:30 +0530

    changing the port from 3000 to 5000

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ const connectToMongo = require("./db");
 const express = require("express");
 connectToMongo();
 const app = express();
-const port = 3000;
-app.use(express.json())
+const port = 5000;
+app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 
 app.use("/api/notes", require("./routes/notes"));
