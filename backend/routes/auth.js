1c3682b73a6055693e359a76eaf31a19e3993749
Himanshu <hv65616@gmail.com>
2022-01-27 22:59:02 +0530

    encrypting password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-
+const bcrypt = require("bcryptjs");
 //Create A User Using POST Request. No login required
 router.post(
   "/createUser",
@@ -22,10 +22,14 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
       res.json({
         nice: "nice",
