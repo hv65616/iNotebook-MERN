ae50eca31009fb0305f4c45b24b0fa2e7916e8be
Himanshu <hv65616@gmail.com>
2022-02-03 20:30:43 +0530

    include react router setup code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import { Home } from "./Components/Home";
+import { About } from "./Components/About";
 function App() {
   return (
-    <div className="App">
-      <h1>This is i NoteBOOK</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
