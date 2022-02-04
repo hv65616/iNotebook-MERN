50661f700841c0c5093fbafc36e760f7c7899b9f
Himanshu <hv65616@gmail.com>
2022-02-04 18:30:32 +0530

    involve the notesstate of context api

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar } from "./Components/Navbar";
 import { Home } from "./Components/Home";
 import { About } from "./Components/About";
+import NotesState from "./context/notes/notesState";
 function App() {
   return (
     <>
-      <Router>
-        <Navbar></Navbar>
-        <Switch>
-          <Route exact path="/home">
-            <Home></Home>
-          </Route>
+      <NotesState>
+        <Router>
+          <Navbar></Navbar>
+          <Switch>
+            <Route exact path="/home">
+              <Home></Home>
+            </Route>
 
-          <Route exact path="/about">
-            <About></About>
-          </Route>
-        </Switch>
-      </Router>
+            <Route exact path="/about">
+              <About></About>
+            </Route>
+          </Switch>
+        </Router>
+      </NotesState>
     </>
   );
 }
