7258ef7ad044df88633dc546c94879c0c3f457d6
Himanshu <hv65616@gmail.com>
2022-02-05 19:56:59 +0530

    remove the one commit before code

--- src/context/notes/notesState.js
+++ src/context/notes/notesState.js
@@ -1,21 +1,9 @@
 import React from "react";
 import NotesContext from "./notesContext";
-import { useState } from "react";
+
 const NotesState = (props) => {
-  const state1 = {
-    name: "Himanshu",
-    class: "5r",
-  };
-  const [state, setState] = useState(state1);
-  const update = () => {
-    setTimeout(() => {
-      setState({ name: "Verma", class: "2r" });
-    }, 1000);
-  };
   return (
-    <NotesContext.Provider value={{ state, update }}>
-      {props.children}
-    </NotesContext.Provider>
+    <NotesContext.Provider value={{}}>{props.children}</NotesContext.Provider>
   );
 };
 
