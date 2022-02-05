f0e3a44d612aa872eb3a509f7fa72a8b47317446
Himanshu <hv65616@gmail.com>
2022-02-05 19:58:11 +0530

    remove all the one commit before code

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,20 +1,9 @@
 import React from "react";
-import { useContext } from "react";
-import notesContext from "../context/notes/notesContext";
-import { useEffect } from "react";
 
 export const About = () => {
-  const a = useContext(notesContext);
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
-
   return (
     <div>
-      <h1>
-        This is about {a.state.name} and he is in class {a.state.class}
-      </h1>
+      <h1>This is about page</h1>
     </div>
   );
 };
