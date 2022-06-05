3beebc68981d3e78e6cb7ae01e9f296fcd84100f
Himanshu <hv65616@gmail.com>
2022-06-05 16:25:45 +0530

    code comments added

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,12 @@
+// Include mongoose into the working state
 const mongoose = require("mongoose");
+
+// It is the ouor monouri which will connect us to mongodb
 const mongoURI =
   "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
-
+  
+// mongoose.connect take two arguments one is mongouri and second is fireback function
+// it connect us to mongodb using mongoose
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("Connected To Mongoose");
